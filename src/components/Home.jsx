6a71242e4f0ebdf8544428a47df20371767b20b5
CristelRR <cristel23rr@gmail.com>
2024-02-27 09:33:56 -0600

    V!

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navigate from './Navigate'
+
+function Home() {
+  return (
+    <div>
+       <Navigate />
+    </div>
+  )
+}
+
+export default Home
+
+
+/** rfce */
+/** rfc */
